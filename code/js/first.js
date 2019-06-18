0e18e5fb68b4f54736ac8def73c46413ebdc75ca
Shiv Sharma <shivsharmavictory@gmail.com>
2019-06-18 21:48:54 +0530

    Update

---
+++ code/js/first.js
@@ -0,0 +1,13 @@
+alert("Hii Welcome To my calculator");
+function trigonometry(x) {
+  var box = document.getElementById('display1');
+  //declaring the values of sin cos Tan
+  var sin = "Math.abs(Math.sin(";
+  var cos = "Math.abs(Math.cos(";
+  var tan = "Math.abs(Math.tan(";
+  if(x=='sin')
+  {
+    box.value +=sin
+  }
+
+}
